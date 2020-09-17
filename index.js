15d0a7c1e52aa8d137fe404deb15a36da69fe078
var-rohit <rohit.cse1551010@gmail.com>
2020-09-17 17:52:17 +0530

    setting up view engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 //use express router
 app.use('/',require('./routes')); // it takes by default index.js 
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
