800abb5fefaf90bf90677f9c84ae63d87e77504b
var-rohit <rohit.cse1551010@gmail.com>
2020-09-17 14:51:33 +0530

    add controller and router

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res){
+    return res.end("Express is up and running for DigitalSocial");
+}
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router
+app.use('/',require('./routes')); // it takes by default index.js 
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
     }
 
-    console.log(`Server is running on port : ${port}`);
+    console.log(`Server is running on port : ${port}`); 
     
 })
