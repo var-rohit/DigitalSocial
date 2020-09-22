d8b32bc8d18164bbf8af510d4f7a60d7bce13ee2
var-rohit <rohit.cse1551010@gmail.com>
2020-09-23 01:31:05 +0530

    added validation in posts and displayed it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,21 @@
+const Post = require('../models/post');
+
+
 module.exports.home = function(req,res){
 
-    console.log(req.cookies);
-    return res.render('home',{
-        title : "DigitalSocial"
-    });
+    //console.log(req.cookies);
+    
+    //populate user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+
+        return res.render('home',{
+            title : "DigitalSocial",
+            posts : posts
+        });
+    
+        });
+
+    
 }
 
 
